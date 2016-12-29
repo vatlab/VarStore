0c571260cb5356d0854a0975b08d2551a7cd2b2f
Ma <jma7@BSTMC07N607BFW5.local>
2016-12-29 16:58:12 -0600

    check pysam

--- pysam_check.py
+++ pysam_check.py
@@ -30,13 +30,32 @@ def getRowFromVCF(file):
     info=list((vcfin.header.info))
     for row in vcfin.fetch():
         ids=row.id.split(";")
+        ooarray=[]
+        zoarray=[]
+        ozarray=[]
+        zzarray=[]
         if len(ids)>1:
             for idx,id in enumerate(ids):
                 print (id,row.contig,row.pos,row.ref,row.alts[idx],row.info["AF"][idx])
-                for sample in row.samples:
-                    print (sample,row.samples[sample].alleles)
-                    for key,value in row.samples[sample].iteritems():
-                        print(key,value)
+            for sample in row.samples:
+                print (sample,row.samples[sample].alleles)
+                for key,value in row.samples[sample].iteritems():
+                    # if idx!=0 and idx in value: 
+                    print(key,value)
+        else:
+            print (ids,row.contig,row.pos,row.ref,row.alts,row.info["AF"])
+            for sample in row.samples:
+                for key,value in row.samples[sample].iteritems():
+                    if (value[0]*value[1]==1):
+                        ooarray.append(sample)      
+                    elif (value[0]==value[1]):
+                        zzarray.append(sample)
+                    elif (value[0]==0):
+                        zoarray.append(sample)
+                    elif (value[0]==1):
+                        ozarray.append(sample)
+            print(len(ooarray),len(zzarray),len(zoarray),len(ozarray))
+                    
             # for key in row.info:
             #     print(key,row.info[key])
         # if len(row.ref)>5:
