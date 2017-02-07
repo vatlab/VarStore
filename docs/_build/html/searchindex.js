c8745b0d001c3d8be234e0a18add67e8c7def06c
Ma <jma7@BSTMC07N607BFW5.local>
2017-02-07 17:04:38 -0600

    test docs

--- docs/_build/html/searchindex.js
+++ docs/_build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({docnames:["index"],envversion:51,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to vatapi&#8217;s documentation!"],titleterms:{document:0,indic:0,tabl:0,vatapi:0,welcom:0}})
+Search.setIndex({docnames:["index"],envversion:51,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{dataaccess_api:0,index:[],modul:[],page:[],search:[],vatdp_api:0,vatvs_api:0},titles:["Welcome to vatAPI&#8217;s documentation!"],titleterms:{document:0,indic:0,tabl:0,vatapi:0,welcom:0}})
